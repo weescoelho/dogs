d03b551d8712a0199882c31f70a7c704c001405d
weescoelho <wes.coelho18@gmail.com>
2021-05-01 10:45:35 -0300

    Implementação do scroll infinito na página principal

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,17 +1,51 @@
-import React from 'react'
-import FeedPhotos from './FeedPhotos/FeedPhotos'
-import FeedModal from './FeedModal/FeedModal'
+import React from "react";
+import FeedPhotos from "./FeedPhotos/FeedPhotos";
+import FeedModal from "./FeedModal/FeedModal";
 
-const Feed = () => {
+const Feed = ({ user }) => {
+  const [modalPhoto, setModalPhoto] = React.useState(null);
+  const [pages, setPages] = React.useState([1]);
+  const [infinite, setInfinite] = React.useState(true);
 
-  const [modalPhoto, setModalPhoto] = React.useState(null)
+  React.useEffect(() => {
+    let wait = false;
+    function infiniteScroll() {
+      if (infinite) {
+        const scroll = window.scrollY; // Total de scroll na página
+        const height = document.body.offsetHeight - window.innerHeight; // total de area que possa dar scroll
+        if (scroll > height * 0.75 && !wait) {
+          setPages((pages) => [...pages, pages.length + 1]);
+          wait = true;
+          setTimeout(() => {
+            wait = false;
+          }, 500);
+        }
+      }
+    }
+    window.addEventListener("wheel", infiniteScroll);
+    window.addEventListener("scroll", infiniteScroll);
+    return () => {
+      window.removeEventListener("wheel", infiniteScroll);
+      window.removeEventListener("scroll", infiniteScroll);
+    };
+  }, [infinite]);
 
   return (
     <div>
-      {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto}/>}
-      <FeedPhotos setModalPhoto={setModalPhoto}/>
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} page="1" setModalPhoto={setModalPhoto} />
+      )}
+      {pages.map((page) => (
+        <FeedPhotos
+          key={page}
+          user={user}
+          page={page}
+          setModalPhoto={setModalPhoto}
+          setInfinite={setInfinite}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { UserContext } from '../../contexts/UserContext'
 import Feed from '../Feed/Feed'
 import UserHeader from './UserHeader/UserHeader'
 import UserPhotoPost from './UserPhotoPost/UserPhotoPost'
 import UserStats from './UserStats/UserStats'
 
 const User = () => {
+
+  const {data} = React.useContext(UserContext)
+
   return (
     <section className="container">
       <UserHeader/>
       <Routes>
-        <Route path="/" element={<Feed/>}/>
+        <Route path="/" element={<Feed user={data.id}/>}/>
         <Route path="postar" element={<UserPhotoPost/>}/>
         <Route path="estatisticas" element={<UserStats/>}/>
       </Routes>
