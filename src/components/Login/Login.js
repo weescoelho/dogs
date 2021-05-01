8286670688e1dd1e76a21763ae5fe3883588080c
weescoelho <wes.coelho18@gmail.com>
2021-05-01 17:13:56 -0300

    Implementação do reset da senha

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -20,7 +20,7 @@ const Login = () => {
         <Route path="/" element={<LoginForm />} />
         <Route path="criar" element={<LoginCreate />} />
         <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="reset" element={<LoginPasswordReset />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
      </div>
--- src/components/Login/LoginPasswordReset/LoginPasswordReset.js
+++ src/components/Login/LoginPasswordReset/LoginPasswordReset.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React from "react";
+import { PASSWORD_RESET } from "../../../api/api";
+import useForm from "../../../hooks/useForm";
+import useFetch from "../../../hooks/useFetch";
+import Button from "../../Forms/Button/Button";
+import Input from "../../Forms/Input/Input";
+import Error from '../../../components/Helper/Error'
+import { useNavigate } from "react-router";
 
 const LoginPasswordReset = () => {
+  const [login, setLogin] = React.useState("");
+  const [key, setKey] = React.useState("");
+  const password = useForm();
+  const { error, loading, request } = useFetch();
+  const navigate = useNavigate()
+
+  React.useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const key = params.get("key");
+    const login = params.get("login");
+    if (key) setKey(key);
+    if (login) setLogin(login);
+  }, []);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = PASSWORD_RESET({
+      login,
+      key,
+      password: password.value,
+    });
+    const { response } = await request(url, options);
+    if(response.ok) navigate('/login')
+  }
+
   return (
     <div>
-      Reset
-    </div>
-  )
-}
+      <h1 className="title">Resete a senha</h1>
 
-export default LoginPasswordReset
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="Nova senha"
+          type="password"
+          name="password"
+          {...password}
+        />
+        {loading ? (
+          <Button disabled>Trocando a senha...</Button>
+        ) : (
+          <Button>Resetar senha</Button>
+        )}
+        <Error error={error} />
+      </form>
+    </div>
+  );
+};
+
+export default LoginPasswordReset;
