dba7cfbf0c5f450d6393643c74843abd8de89604
weescoelho <wes.coelho18@gmail.com>
2021-05-03 21:09:15 -0300

    Implementado Lazy/Suspense no componente de graficos para otimização

--- src/components/User/UserStats/UserStats.js
+++ src/components/User/UserStats/UserStats.js
@@ -4,7 +4,7 @@ import useFetch from "../../../hooks/useFetch";
 import Error from "../../Helper/Error";
 import Head from "../../Helper/Head/Head";
 import Loading from "../../Helper/Loading/Loading";
-import UserStatsGraphs from "../UserStatsGraphs/UserStatsGraphs";
+const UserStatsGraphs = React.lazy(() => import('../UserStatsGraphs/UserStatsGraphs'))
 
 const UserStats = () => {
   const { data, error, loading, request } = useFetch();
@@ -21,10 +21,10 @@ const UserStats = () => {
   if (error) return <Error error={error} />;
   if (data)
     return (
-      <div>
+      <React.Suspense fallback={<div></div>}>
         <Head title="Estatísticas" />
         <UserStatsGraphs data={data}/>
-      </div>
+      </React.Suspense>
     );
   else return null;
 };
